0773e515e04a644c72238dbc67a00628d0a708bc
arifsyahrudin <arifsyah1018@gmail.com>
2023-11-01 22:14:50 +0700

    sideMenu

--- js/script.js
+++ js/script.js
@@ -16,37 +16,37 @@ window.addEventListener("scroll", () => {
 // menu-button
 const navbarNav = document.querySelector(".nav-list");
 const menuButton = document.querySelector("#menu-button");
-const cancelButton = document.querySelector("#cancel-button");
+const menuSvg = document.querySelector(".menu-svg");
 
 menuButton.onclick = (e) => {
-  navbarNav.classList.toggle("active");
-  menuButton.style.display = "none";
-  cancelButton.style.display = "inline-block";
-  e.preventDefault();
-};
-
-cancelButton.onclick = (e) => {
-  navbarNav.classList.remove("active");
-  menuButton.style.display = "inline-block";
-  cancelButton.style.display = "none";
+  if (navbarNav.classList.contains("active")) {
+    navbarNav.classList.remove("active");
+    menuSvg.style.backgroundColor = "transparent";
+    menuSvg.style.boxShadow = "none";
+    menuButton.style.color = "#1e1c1c";
+  } else {
+    navbarNav.classList.add("active");
+    menuSvg.style.boxShadow = "2px 2px 0 1px rgba(200,0,0,.5)";
+    menuSvg.style.backgroundColor = "#1e1c1c";
+    menuButton.style.color = "#fee506";
+  }
   e.preventDefault();
 };
 
 function checkScreenWidth() {
   if (window.innerWidth <= 898) {
     menuButton.style.display = "inline-block";
-    cancelButton.style.display = "none";
   } else {
     menuButton.style.display = "none";
-    cancelButton.style.display = "none";
   }
 }
 
 document.addEventListener("click", function (e) {
   if (navbarNav.classList.contains("active") && !menuButton.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
-    cancelButton.style.display = "none";
-    menuButton.style.display = "block";
+    menuSvg.style.backgroundColor = "transparent";
+    menuButton.style.color = "#1e1c1c";
+    menuSvg.style.boxShadow = "none";
   }
 });
 
